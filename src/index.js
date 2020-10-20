4c35b620941da1b806d776cb87d123dc123cdd7d
eresha1231 <eresha1231@gmail.com>
2020-10-20 21:52:27 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsArr) {
+  while(str !== '') {    
+    let flag = false;
+    bracketsArr.forEach(element => { 
+      let brackets = element[0] + element[1]; 
+        if (str.includes(brackets)){ 
+            str = str.replace(brackets, ''); 
+            flag = true;
+        }
+    });
+    if (flag === false) break;
+  }
+  return (str === '') ? true  : false;
 }
+
